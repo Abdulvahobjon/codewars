cae9e4f2c8e45668ce420eda4b1ac1ee41b9c4f7
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-10-03 20:22:44 +0500

    code6

---
+++ code6.js
@@ -0,0 +1,6 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+console.log(goals(12 , 34 , 56));
