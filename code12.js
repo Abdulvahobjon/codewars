ec546f1629e639f4cbe836b1d84bc6a75ba47e33
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-11-02 07:28:28 +0500

    add 12

--- code12.js
+++ code12.js
@@ -1,5 +1,12 @@
-function solution(str){
-    return str.split("").reverse().join("")
+function openOrSenior(data){
+  return  data.map(item=>{
+        if(item[0] >= 55 && item[1] > 7){
+            return "Senior"
+        } else{
+            return "Open"
+        }
+    })
 }
 
-console.log((solution("javascript")));
+
+console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]));
